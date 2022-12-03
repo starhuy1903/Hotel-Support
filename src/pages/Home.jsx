9fb8642810fc991edcfa81db5b669990a997352d
starhuy1903 <huykst137@gmail.com>
2022-12-03 19:10:18 +0700

    feat: create FeaturedProperties component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Featured from "../components/Featured";
+import FeaturedProperties from "../components/FeaturedProperties";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import PropertyList from "../components/PropertyList";
@@ -8,17 +9,14 @@ const Home = () => {
   return (
     <div className="flex flex-col">
       <Header />
-      {/* homeContainer */}
       <div className="flex flex-col items-center mt-12 gap-8">
         <Featured />
-        {/* homeTitle */}
         <h1 className="w-[1024px] text-xl font-bold">
           Browse by property type
         </h1>
         <PropertyList />
-        {/*  */}
         <h1 className="w-[1024px] text-xl font-bold">Homes guests love</h1>
-        {/* <FeaturedProperties /> */}
+        <FeaturedProperties />
         {/* <MailList /> */}
         <Footer />
       </div>

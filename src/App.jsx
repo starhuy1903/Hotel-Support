43d50a14f9c5956ecc9021b7fc174b15022fe7a4
starhuy1903 <huykst137@gmail.com>
2022-12-04 12:36:00 +0700

    feat: create Hotel Detail page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import RequireAuth from "./features/auth/RequireAuth";
 import Home from "./pages/Home";
+import HotelDetail from "./pages/HotelDetail";
 import HotelList from "./pages/HotelList";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -15,6 +16,7 @@ function App() {
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
         <Route path="hotels" element={<HotelList />} />
+        <Route path="hotels/:hotelId" element={<HotelDetail/>} />
 
         {/*  protected routes*/}
         <Route element={<RequireAuth />}></Route>
